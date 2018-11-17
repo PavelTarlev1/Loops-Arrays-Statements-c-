6d31e73f571a222a64081745016f1d2f0eb53655
PavelTarlev1 <32453996+PavelTarlev1@users.noreply.github.com>
2018-11-17 16:16:00 +0200

    Add files via upload

---
+++ Най-малка Цифра.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication149
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Number = int.Parse(Console.ReadLine());
+            var arr = Number.ToString().Select(t => int.Parse(t.ToString())).ToArray();
+            Array.Sort(arr);
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if(arr[i]!=0)
+                {
+                    Console.WriteLine(arr[i]);
+                    break;
+                }
+            }
+
+        }
+    }
+}
---
+++ Прозорци.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication151
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string NumberPrice = Console.ReadLine();
+            string [] y = NumberPrice.Split(' ');
+            int Number = int.Parse(y[0]);
+            int Prise = int.Parse(y[1]);
+            int result = 0;
+            for (int i = 0; i < Number; i++)
+            {
+                int[] arr = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+                if (arr[2] == 1)
+                {
+                    result += (arr[0] * arr[1]);
+                }
+            }
+            Console.WriteLine(result*Prise);
+        }
+    }
+}
