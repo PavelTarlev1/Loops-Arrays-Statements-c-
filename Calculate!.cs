40c38cb683f1f9aaaa5268c89fb3dbd0b12928be
PavelTarlev1 <32453996+PavelTarlev1@users.noreply.github.com>
2018-11-20 14:06:57 +0200

    Add files via upload

---
+++ Calculate!.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication30
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double n = double.Parse(Console.ReadLine());
+            double x = double.Parse(Console.ReadLine());
+            double result = 1;
+            long ifac = 1;
+            for (int i = 1; i <= n; i++)
+            {
+                ifac = 1;
+                for (int j = i; j >= 1; j--)
+                {
+                    ifac *= j;
+                }
+                result += ifac / Math.Pow(x, i);
+            }
+            Console.WriteLine("{0:F5}", result);
+
+        }
+    }
+}
---
+++ Огледални Кодове.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication161
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string x = Console.ReadLine();
+            string [] y = x.Split(' ');
+            int firstNumber = 0;
+            int secondNumber = 0;
+            var arrFirst = y[0].Select(t => int.Parse(t.ToString())).ToArray();
+            var arrSecond = y[1].Select(t => int.Parse(t.ToString())).ToArray();
+            
+            Array.Reverse(arrFirst);
+            Array.Reverse(arrSecond);
+            
+            for (int i = 0; i < arrFirst.Length; i++)
+            {
+                firstNumber += arrFirst[i] * Convert.ToInt32(Math.Pow(10, arrFirst.Length - i - 1));
+                secondNumber += arrSecond[i] * Convert.ToInt32(Math.Pow(10, arrSecond.Length - i - 1));
+                
+            }
+            int result = Math.Max(firstNumber,secondNumber);
+            Console.WriteLine(result);
+
+        }
+    }
+}
