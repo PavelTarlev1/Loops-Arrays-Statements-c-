a76ed8bdc89383054b4c175fedb8d7a83918e86a
PavelTarlev1 <32453996+PavelTarlev1@users.noreply.github.com>
2018-11-28 22:04:50 +0200

    Add files via upload

---
+++ Odd or Even Product.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication171
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int HLArr = int.Parse(Console.ReadLine());
+            int[] arr = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            long odd = 1,
+                even = 1;
+
+            for (int i = 0; i < HLArr; i++)
+            {
+                if (i == 0)
+                {
+                    odd *= arr[i];
+                    continue;
+                }
+                if (i % 2 != 0)
+                {
+                    even *= arr[i];
+                    continue;
+                }
+                if (i % 2 == 0)
+                {
+                    odd *= arr[i];
+                    continue;
+                }
+                
+            }
+            if (odd == even)
+            {
+                Console.WriteLine("yes {0}",odd);
+            }
+            if (odd != even)
+            {
+                Console.WriteLine("no {0} {1}",odd , even);
+            }
+        }
+    }
+}
---
+++ Prime Triangle.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication164
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            string result = "";
+            int counter = 0;
+            for (int i = 1; i < number + 1; i++)
+            {
+                bool IsPrime = true;
+                for (int p = 2; p < i; p++)
+                {
+                    if (i % p == 0)
+                    {
+                        IsPrime = false;
+                    }
+                }
+                if (IsPrime == true)
+                {
+                    result += "1";
+                    counter++;
+                    Console.WriteLine(result);
+                }
+                if (IsPrime == false)
+                {
+                    result += "0";
+                }
+            }
+        }
+    }
+}
+
